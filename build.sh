95d1dee29acf726a8904ca64607ce36aa5a6dc01
olbrew <olbrew@users.noreply.github.com>
2016-04-29 23:38:42 +0200

    add build script to build the lexer and parser

---
+++ build.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+java -jar lib/antlr-4.5.3-complete.jar -Dlanguage=Python3 src/grammar/SmallC.g4
+
+# tests
+#python3 src/compiler/C2P.py test/easterfiles/good.c output.p
+
+#echo "Testing bad.c files. These should give errors"
+#python3 src/compiler/C2P.py test/easterfiles/bad1.c output.p
+#python3 src/compiler/C2P.py test/easterfiles/bad2.c output.p
+#python3 src/compiler/C2P.py test/easterfiles/bad3.c output.p
