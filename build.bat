822b59cf2aec97f68987fe1dc28404980115cd7f
puntje <fc.puntje@gmail.com>
2016-05-03 14:27:02 +0200

    Add visitor flag to build scripts

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-java -jar lib/antlr-4.5.3-complete.jar -Dlanguage=Python3 src/grammar/SmallC.g4
+java -jar lib/antlr-4.5.3-complete.jar -visitor -Dlanguage=Python3 src/grammar/SmallC.g4
 
 :: tests
 ::python3 src/compiler/C2P.py test/easterfiles/good.c output.p
