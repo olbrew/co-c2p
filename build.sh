32c1e205816c5f1e43a2dce06efc6037cf48f9b9
olbrew <olbrew@users.noreply.github.com>
2016-05-18 12:13:34 +0200

    Update build scripts
    Now they install dependencies and actually run our test files.
    Also add some info for the user.

--- build.sh
+++ build.sh
@@ -1,11 +1,23 @@
 #! /bin/bash
 
+## Build
+# Install antlr python bindings if they're not yet installed
+pip3 install antlr4-python3-runtime==4.5.3
+printf "\n"
+
+# Build Grammar Lexers and Parsers with antlr4
 java -jar lib/antlr-4.5.3-complete.jar -visitor -Dlanguage=Python3 src/grammar/SmallC.g4
+printf "\n"
+printf "\n"
 
-# tests
-#python3 src/compiler/C2P.py test/easterfiles/good.c output.p
+## tests
+printf "Testing \`test/good.c\`, this should work."
+python3 src/C2P.py test/good.c output.p
+printf "\n"
 
-#echo "Testing bad.c files. These should give errors"
-#python3 src/compiler/C2P.py test/easterfiles/bad1.c output.p
-#python3 src/compiler/C2P.py test/easterfiles/bad2.c output.p
-#python3 src/compiler/C2P.py test/easterfiles/bad3.c output.p
+echo "Testing \`test/bad.c\` files, these should give errors"
+python3 src/C2P.py test/bad1.c output.p
+printf "\n"
+python3 src/C2P.py test/bad2.c output.p
+printf "\n"
+python3 src/C2P.py test/bad3.c output.p
