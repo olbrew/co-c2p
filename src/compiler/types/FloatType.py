df04cace7cb589253b6fa4ab67fecb3ea51e3316
puntje <fc.puntje@gmail.com>
2016-05-31 12:09:46 +0200

    literal 'f' is already removed in ASTGenerator.

--- src/compiler/types/FloatType.py
+++ src/compiler/types/FloatType.py
@@ -19,7 +19,4 @@ class FloatType(Type):
         return other_type
 
     def literalToPCode(self, literal):
-        # a float may end on explicit character 'f' in smallC
-        if literal[-1] is 'f':
-            return str(literal[:-1])
         return str(literal)
