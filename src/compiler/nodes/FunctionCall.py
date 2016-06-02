1c9e416980779d0507fee12005538afa184cf911
Eduard <fc.puntje@gmail.com>
2016-06-02 17:38:35 +0200

    Checks for forward declaration
    Fixes #9 for function identifiers; variable identifiers have been
    previously checked in 810db9e

--- src/compiler/nodes/FunctionCall.py
+++ src/compiler/nodes/FunctionCall.py
@@ -6,6 +6,14 @@ class FunctionCall(Expression):
     def __init__(self, ast, identifier, parameters):
         super().__init__(ast)
         self.identifier = identifier
+        
+        found_identifier = False
+        for scope in ast.symbol_table.stack:
+            if self.identifier in scope:
+                found_identifier = True
+        if not found_identifier:
+            raise Exception("Syntax error: function", self.identifier, "is called before declaration / definition.")
+        
         self.parameter_list = parameters
         self.addChild(self.parameter_list)
         self.result_type = ast.symbol_table.getSymbol(self.identifier).type
