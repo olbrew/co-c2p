c5230e947c4b0c24bd51ae2fc91f1fd562ed4726
olbrew <olbrew@users.noreply.github.com>
2016-06-10 23:12:48 +0200

    use pip instead of pip3 on windows

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo OFF
 
 :: Install antlr python bindings if they're not yet installed
-pip3 install antlr4-python3-runtime==4.5.3
+pip install antlr4-python3-runtime==4.5.3
 
 :: Build Grammar Lexers and Parsers with antlr4
 ECHO "Building `smallC` grammar lexers and parsers"
