b13f280b428a3ec399894e82b2f739939127d011
olbrew <olbrew@users.noreply.github.com>
2016-06-10 19:16:20 +0200

    update test scripts to loop over test files

--- test.sh
+++ test.sh
@@ -1,12 +1,18 @@
-#! /bin/bash
+#! /bin/sh
 
-printf "Testing \`test/good.c\`, this should work.\n"
-python3 src/C2P.py tests/good.c output.p
+printf "Testing good c files. These should work.\n"
+for GOOD in tests/good/*.c
+do
+        printf "Testing %s ...\n" "$GOOD"
+        python3 c2p/C2P.py "$GOOD" output.p
+        printf "SUCCES\n\n"
+done
 printf "\n"
 
-printf "Testing \`test/bad.c\` files, these should give errors\n"
-python3 src/C2P.py tests/bad1.c output.p
-printf "\n"
-python3 src/C2P.py tests/bad2.c output.p
-printf "\n"
-python3 src/C2P.py tests/bad3.c output.p
+printf "Testing bad c files. These should give errors.\n"
+for BAD in tests/bad/*.c
+do
+        printf "Testing %s ...\n" "$BAD"
+        python3 c2p/C2P.py "$BAD" output.p
+        printf "\n"
+done
